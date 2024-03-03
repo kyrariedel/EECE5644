086b1cf1825171db09f0ac030125488a08df0a12
Kyra Riedel <kyra.riedel@gmail.com>
2024-03-03 00:02:48 -0500

    hw3 fix

--- LassoSweepAlpha.py
+++ LassoSweepAlpha.py
@@ -6,13 +6,8 @@ import matplotlib.pyplot as plt
 from data_generator import postfix
 from data_generator import liftDataset
 
-# Number of samples
 N = 1000
-
-# Noise variance 
 sigma = 0.01
-
-# Feature dimension
 d = 40
 
 psfx = postfix(N, d, sigma)
@@ -22,19 +17,15 @@ y = np.load("y" + psfx + ".npy")
 
 print("Dataset has n=%d samples, each with d=%d features," % X.shape, "as well as %d labels." % y.shape[0])
 
-# Lift the dataset
 X_lifted = liftDataset(X)
 
-# Split the lifted dataset to training and test sets
 X_train_lifted, X_test_lifted, y_train, y_test = train_test_split(
     X_lifted, y, test_size=0.30, random_state=42)
 
 print("Randomly split lifted dataset to %d training and %d test samples" % (X_train_lifted.shape[0], X_test_lifted.shape[0]))
 
-# Define a range of alpha values
 alphas = 2.0**np.arange(-10, 11)
 
-# Perform 5-fold cross-validation to find the optimal alpha
 cv_rmse_mean = []
 cv_rmse_std = []
 
@@ -44,22 +35,19 @@ for alpha in alphas:
     cv_rmse_mean.append(np.sqrt(-np.mean(cv_scores)))
     cv_rmse_std.append(np.std(np.sqrt(-cv_scores)))
 
-# Find the optimal alpha that minimizes mean RMSE
 optimal_alpha = alphas[np.argmin(cv_rmse_mean)]
 print("Optimal Alpha:", optimal_alpha)
 
-# Plot cross-validation mean RMSE as a function of alpha
 plt.errorbar(np.log2(alphas), cv_rmse_mean, yerr=cv_rmse_std, marker='o')
 plt.title('Cross-Validation Mean RMSE as a function of Alpha')
 plt.xlabel('log2(Alpha)')
 plt.ylabel('Cross-Validation Mean RMSE')
 plt.show()
 
-# Retrain Lasso model on the entire training set with the optimal alpha
+
 optimal_model = Lasso(alpha=optimal_alpha)
 optimal_model.fit(X_train_lifted, y_train)
 
-# Compute RMSE on training and test sets
 rmse_train = np.sqrt(mean_squared_error(y_train, optimal_model.predict(X_train_lifted)))
 rmse_test = np.sqrt(mean_squared_error(y_test, optimal_model.predict(X_test_lifted)))
 
